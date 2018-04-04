d948e19e4afadc66c2d6d1be9cdc4293b90471b2
Brady <Jorl@Bradys-MacBook-Pro.local>
2018-04-04 18:09:28 -0500

    First commit

---
+++ RedditScrape/build/lib/RedditScrape/spiders/redditbot.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+class RedditbotSpider(scrapy.Spider):
+	name = 'redditbot'
+	allowed_domains = ['www.reddit.com']
+	start_urls = ['http://www.reddit.com/']
+
+	def parse(self, response):
+		#extracting content from site
+		subreddit = response.css('.subreddit.hover.may-blank::text').extract()
+		title = response.css('.title.may-blank::text').extract()
+
+		#Processing extracted info
+		for item in zip(subreddit, title):
+			#store info in dictionary
+			scraped_info = {
+				'subreddit' : item[0],
+				'title' : item[1]
+			}
+
+			#yield scraped info to scrapy
+			yield scraped_info
