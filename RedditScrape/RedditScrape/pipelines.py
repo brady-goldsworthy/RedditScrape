3dff03c5e6ba534829a4f213f68d734d48a8e7d9
Brady <Jorl@Bradys-MacBook-Pro.local>
2018-05-06 16:21:03 -0500

    Added post id xpath selector

--- RedditScrape/RedditScrape/pipelines.py
+++ RedditScrape/RedditScrape/pipelines.py
@@ -6,6 +6,6 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class RedditscrapePipeline(object):
+class RedditScrapePipeline(object):
     def process_item(self, item, spider):
         return item
